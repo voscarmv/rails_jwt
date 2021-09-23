3721cc10f61064432896082e331a6b2f25f05904
Oscar Mier <voscarmv@gmail.com>
2021-09-22 23:11:16 -0500

    Implemented proper jti auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+    skip_before_action :verify_authenticity_token
     def authenticate_user
         if request.headers['Authorization'].present?
             authenticate_or_request_with_http_token do |token|
                 begin
                     jwt_payload = JWT.decode(token, Rails.application.credentials.jwt_secret).first
                     @current_user_id = jwt_payload['jti']
+                    User.find_by(jti: @current_user_id)
                 rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
                     head :unauthorized
                 end
